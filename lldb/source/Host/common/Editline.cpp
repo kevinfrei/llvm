d32f0208efee165dd533cd6b0595c98490db9f10
Alexandre Perez <alexandreperez@meta.com>
2022-11-22 14:58:04 -0800

    Register complete and rl_complete libedit callbacks
    Summary: As titled.
    
    Test Plan:
    Created this `~/.editrc`:
    ```
    bind ^I complete
    bind \\t rl_complete
    ```
    
    Then ran the newly-built lldb binary. It no longer crashes:
    ```
    [alexandreperez@devvm7392.ftw0 ~/local/lldb-sand]$ ./install/Prod/usr/bin/lldb
    (lldb)
    ```
    
    Reviewers: gclayton, wanyi, #lldb_team
    
    Reviewed By: wanyi
    
    Subscribers: #lldb_team
    
    Differential Revision: https://phabricator.intern.facebook.com/D41483566

--- lldb/source/Host/common/Editline.cpp
+++ lldb/source/Host/common/Editline.cpp
@@ -1248,6 +1248,12 @@ void Editline::ConfigureEditor(bool multiline) {
   AddFunctionToEditLine(EditLineConstString("lldb_complete"),
                         EditLineConstString("Invoke completion"),
                         complete_callback);
+  AddFunctionToEditLine(EditLineConstString("rl_complete"),
+                        EditLineConstString("Invoke completion"),
+                        complete_callback);
+  AddFunctionToEditLine(EditLineConstString("complete"),
+                        EditLineConstString("Invoke completion"),
+                        complete_callback);
 
   // General bindings we don't mind being overridden
   if (!multiline) {
